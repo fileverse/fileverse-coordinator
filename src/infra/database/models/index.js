2af06b057869282315f3d99545c2a5dbcdca2538
Aakash-Jakhmola <aakash001jakhmola@gmail.com>
2023-09-19 12:42:27 +0530

    add notification schema

--- src/infra/database/models/index.js
+++ src/infra/database/models/index.js
@@ -2,6 +2,7 @@ require('../');
 
 const _models = {
   Message: require('./message').model,
+  Notification: require('./notification').model,
 };
 
 module.exports = _models;
---
+++ src/infra/database/models/notification.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const _message = require('./message');
+const { Schema } = mongoose;
+
+const _notification = {};
+
+_notification.schema = new Schema({
+  portalAddress: {
+    type: String,
+    lowercase: true,
+    trim: true,
+  },
+  forAddress: [{ type: String, lowercase: true, trim: true }],
+  audience: {
+    type: String,
+    enum: ['collaborators_only', 'members_only', 'individuals'],
+  },
+  content: {
+    type: String,
+    trim: true,
+  },
+  processed: { type: Boolean, default: false },
+  timeStamp: { type: Date, required: true, default: Date.now },
+});
+
+_notification.schema.pre('save', function (next) {
+  this.timeStamp = Date.now();
+  next();
+});
+
+_notification.schema.methods.safeObject = function () {
+  const safeFields = [
+    '_id',
+    'portalAddress',
+    'forAddress',
+    'audience',
+    'content',
+    'processed',
+    'timeStamp',
+  ];
+  const newSafeObject = {};
+  safeFields.forEach((elem) => {
+    // eslint-disable-next-line security/detect-object-injection
+    newSafeObject[elem] = this[elem];
+  });
+  return newSafeObject;
+};
+
+_message.model = mongoose.model('notifications', _notification.schema);
+
+module.exports = _message;
