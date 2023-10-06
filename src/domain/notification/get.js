e8a523970a1bc0c747c3a823036de882dc7c557b
Aakash-Jakhmola <aakash001jakhmola@gmail.com>
2023-10-06 18:22:11 +0530

    fix get notifications

--- src/domain/notification/get.js
+++ src/domain/notification/get.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const config = require('../../../config');
 const { Notification } = require('../../infra/database/models');
+const formatAddress = require('../portal/formatAddress');
 
 async function get({ address, read, offset, limit, portalAddress }) {
   const matchQuery = {};
@@ -23,10 +24,19 @@ async function get({ address, read, offset, limit, portalAddress }) {
 
   notifications.forEach((notification, index) => {
     notification = notification.safeObject();
-    if (notification.message.includes(address)) {
-      notification.message = notification.message.replace(address, 'you');
-      notifications[index] = notification;
-    }
+    const words = notification.message.split(' ');
+    let newWords = [];
+    words.forEach((word, index) => {
+      let newWord = word;
+      if (word === address) {
+        newWord = index === 0 ? 'You' : 'you';
+      } else if (word.startsWith('0x')) {
+        newWord = formatAddress(word);
+      }
+      newWords.push(newWord);
+    });
+    notification.message = newWords.join(' ');
+    notifications[index] = notification;
   });
 
   return notifications;
