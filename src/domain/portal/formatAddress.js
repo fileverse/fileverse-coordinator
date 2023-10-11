5ed8125c50f3b8db8a4e598ff200759fa6d4937e
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-10-12 01:44:53 +0530

    Minor fix of only showing initial four and last four character of address

--- src/domain/portal/formatAddress.js
+++ src/domain/portal/formatAddress.js
@@ -1,6 +1,6 @@
 function formatAddress(address) {
   if (!address) return '';
-  return address.slice(0, 5) + '...' + address.slice(address.length - 5);
+  return address.slice(0, 4) + '...' + address.slice(address.length - 4);
 }
 
 module.exports = formatAddress;
