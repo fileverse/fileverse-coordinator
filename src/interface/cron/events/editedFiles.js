bf385d4bf3b9cb3b23ae9a518ca33796b7521f73
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-10-23 01:31:56 +0530

    Added job for editedFiles

--- src/interface/cron/events/editedFiles.js
+++ src/interface/cron/events/editedFiles.js
@@ -1,176 +1,125 @@
-const jobs = require('../jobs');
-const agenda = require('./../index');
-const axios = require('axios');
-const config = require('./../../../../config');
-const {
-  EventProcessor,
-  Portal,
-  Notification,
-} = require('../../../infra/database/models');
-const getFileDetails = require('../../../domain/notification/file/getFileDetails');
-const getPortalMetadata = require('../../../domain/portal/getPortalMetadata');
-const formatAddress = require('../../../domain/portal/formatAddress');
+const config = require("../../../../config");
+const { EventProcessor, Event } = require("../../../infra/database/models");
+const agenda = require("../index");
+const jobs = require("../jobs");
+const axios = require("axios");
 
-const apiURL = config.SUBGRAPH_API;
+const API_URL = config.SUBGRAPH_API;
+const STATUS_API_URL = config.SUBGRAPH_STATUS_API;
 
-function getNotificationType(whatSource) {
-  switch (whatSource) {
-    case 'dPage':
-      return 'dPageEdit';
-    case 'dDoc':
-      return 'dDocEdit';
-    case 'file':
-      return 'editFile';
-    case 'whiteboard':
-      return 'whiteboardEdit';
-  }
-}
+const EVENT_NAME = "editedFiles";
+const BATCH_SIZE = 10;
 
-function getFileEditMessage({ whoEdited, whatSource, whatEdited }) {
-  let message = '';
-  if (whatSource === 'dPage') {
-    if (whatEdited) {
-      message = `${whoEdited} edited  "${whatEdited}" dPage`;
-    } else {
-      message = `${whoEdited} edited a dPage`;
-    }
-  } else if (whatSource === 'whiteboard') {
-    if (whatEdited) {
-      message = `${whoEdited} edited "${whatEdited}" whiteboard`;
-    } else {
-      message = `${whoEdited} uploaded a whiteboard `;
-    }
-  } else if (whatSource === 'dDoc') {
-    if (whatEdited) {
-      message = `${whoEdited} edited "${whatEdited}" dDoc`;
-    } else {
-      message = `${whoEdited} edited a dDoc"`;
-    }
-  } else {
-    if (whatEdited) {
-      message = `${whoEdited} edited file "${whatEdited}"`;
-    } else {
-      message = `${whoEdited} edited a file`;
-    }
-  }
-  return message;
-}
-
-agenda.define(jobs.EDITED_FILE, async (job, done) => {
+agenda.define(jobs.ADDED_FILE, async (job, done) => {
   try {
-    const eventName = 'editedFiles';
-    const eventProcessed = await EventProcessor.findOne({});
-    let editedFileCheckpt = 0;
-    if (eventProcessed) {
-      editedFileCheckpt = eventProcessed.editFile;
-    }
-    const editFileData = await axios.post(apiURL, {
-      query: `{
-      ${eventName}(first : 5, orderDirection: asc, orderBy: blockNumber, where:{blockNumber_gt: ${editedFileCheckpt}}) {
-        fileType,
-        metadataIPFSHash,
-        blockNumber,
-        by,
-        fileId,
-        portalAddress,
-        portalMetadataIPFSHash
-      }
-    }`,
-    });
-
-    const data = editFileData?.data?.data;
-    const editedFiles = data[eventName];
-    let newEditedFileCheckpt = null;
-    if (editedFiles && editedFiles.length) {
-      newEditedFileCheckpt = editedFiles.slice(-1)[0].blockNumber;
-    }
-
-    console.log('Recieved entries', jobs.EDITED_FILE, editedFiles.length);
-
-    await Promise.all(
-      editedFiles.map(async (editFile) => {
-        const portal = await Portal.findOne({
-          portalAddress: editFile.portalAddress,
-        });
-        const fileDetails = await getFileDetails({
-          portal,
-          fileTypeNumber: editFile.fileType,
-          metadataIPFSHash: editFile.metadataIPFSHash,
-        });
-
-        const notifications = await Notification.find({
-          portalAddress: editFile.portalAddress,
-          'content.fileId': editFile.fileId,
-        })
-          .sort({ blockNumber: -1 })
-          .limit(1);
-
-        const prevFileMetadata =
-          notifications.length && notifications[0]?.content?.fileMetadata;
-
-        const portalDetails = await getPortalMetadata({
-          portal,
-          portalMetadataIPFSHash: editFile.portalMetadataIPFSHash,
-        });
-
-        if (!fileDetails.forAddress.includes(editFile.by)) {
-          fileDetails.forAddress.push(editFile.by);
-        }
-
-        const notif = new Notification({
-          portalAddress: editFile.portalAddress,
-          audience: fileDetails.audience,
-          forAddress: fileDetails.forAddress,
-          blockNumber: editFile.blockNumber,
-          type: getNotificationType(fileDetails.metadata.source),
-          message: getFileEditMessage({
-            whoEdited: editFile.by,
-            whatSource: fileDetails.metadata.source,
-            whatEdited: fileDetails?.metadata?.name,
-          }),
-          content: {
-            by: editFile.by,
-            metadataIPFSHash: editFile.metadataIPFSHash,
-            fileType: editFile.fileType,
-            fileMetadata: fileDetails.metadata,
-            fileId: editFile.fileId,
-            portalLogo: portal.logo,
-            portalName: portal.name,
-          },
-        });
-
-        if (
-          editFile?.metadataIPFSHash ===
-          'bafybeify3xbts44jrrcidno7gxqs5fyvf5rbx3zkncnbjaibejjetvqtqe/metadata'
-        ) {
-          notif.type = 'deleteFile';
-          let message = `${editFile.by} deleted the ${fileDetails.metadata.source}`;
-          if (prevFileMetadata && prevFileMetadata.name) {
-            message += ` "${prevFileMetadata.name}"`;
-          }
-          notif.message = message;
-        }
-
-        await notif.save();
-      }),
+    const latestBlockNumber = await getLatestBlockNumberFromSubgraph();
+    const editedFilesCheckpoint = await fetchEditedFilesCheckpoint();
+    const batchSize = BATCH_SIZE;
+    const editedFiles = await fetchEditedFilesEvents(
+      editedFilesCheckpoint,
+      batchSize
     );
-
-    if (newEditedFileCheckpt) {
-      await EventProcessor.updateOne(
-        {},
-        {
-          $set: {
-            editFile: newEditedFileCheckpt,
-          },
-        },
-        { upsert: true },
-      );
+    console.log("Received entries", jobs.ADDED_FILE, editedFiles.length);
+    await processEditedFilesEvents(editedFiles);
+    const lastEditedFilesCheckpoint = getLastEditedFilesCheckpoint({
+      editedFiles,
+      batchSize,
+      latestBlockNumber,
+    });
+    if (lastEditedFilesCheckpoint) {
+      await updateEditedFilesCheckpoint(lastEditedFilesCheckpoint);
     }
     done();
   } catch (err) {
-    console.error('error during job', jobs.EDITED_FILE, err);
+    console.error("Error in job", jobs.ADDED_FILE, err);
     done(err);
   } finally {
-    console.log('Done job', jobs.EDITED_FILE);
+    console.log("Job done", jobs.ADDED_FILE);
   }
 });
+
+async function getLatestBlockNumberFromSubgraph() {
+  const response = await axios.get(STATUS_API_URL);
+  const statusObject =
+    response?.data?.data["indexingStatusForCurrentVersion"] || {};
+  const chains = statusObject.chains || [];
+  const firstObject = chains.pop();
+  return parseInt(firstObject?.latestBlock?.number, 10) || 0;
+}
+
+async function fetchEditedFilesCheckpoint() {
+  const eventProcessed = await EventProcessor.findOne({});
+  return eventProcessed ? eventProcessed.editedFiles : 0;
+}
+
+async function fetchEditedFilesEvents(checkpoint, itemCount) {
+  const response = await axios.post(API_URL, {
+    query: `{
+      ${EVENT_NAME}(first: ${
+      itemCount || 5
+    }, orderDirection: asc, orderBy: blockNumber, where: { blockNumber_gte : ${checkpoint} }) {
+        id,
+        fileId,
+        fileType,
+        metadataIPFSHash,
+        contentIPFSHash,
+        gateIPFSHash,
+        by,
+        blockNumber,
+        blockTimestamp,
+        portalAddress  
+      }
+    }`,
+  });
+  return response?.data?.data[EVENT_NAME] || [];
+}
+
+async function processEditedFilesEvent(addedFile) {
+  try {
+    const event = new Event({
+      eventName: EVENT_NAME,
+      data: addedFile,
+      uuid: addedFile.id,
+      portalAddress: addedFile.portalAddress,
+      blockNumber: addedFile.blockNumber,
+      jobName: jobs.ADDED_FILE,
+      blockTimestamp: addedFile.blockTimestamp,
+    });
+    await event.save();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function processEditedFilesEvents(editedFiles) {
+  const allPromises = editedFiles.map(async (editedFiles) => {
+    await processEditedFilesEvent(editedFiles);
+  });
+  const data = await Promise.all(allPromises);
+  return data;
+}
+
+function getLastEditedFilesCheckpoint({
+  editedFiles,
+  batchSize,
+  latestBlockNumber,
+}) {
+  if (editedFiles.length < batchSize) {
+    return latestBlockNumber;
+  }
+  const lastElem = (editedFiles || []).pop();
+  return lastElem ? lastElem.blockNumber : null;
+}
+
+function updateEditedFilesCheckpoint(newCheckpoint) {
+  if (!newCheckpoint || newCheckpoint < 0) return;
+  return EventProcessor.updateOne(
+    {},
+    {
+      $set: {
+        editedFiles: newCheckpoint,
+      },
+    },
+    { upsert: true }
+  );
+}
