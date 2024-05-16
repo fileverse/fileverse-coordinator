bf2ba4e3fe3f913c18ab67726c21c42c38720b72
Nadeem <nadeem@fileverse.io>
2024-05-17 01:41:56 +0530

    fix secret key header value

--- src/interface/cron/events/publicPortalIndex.js
+++ src/interface/cron/events/publicPortalIndex.js
@@ -3,35 +3,37 @@ const agenda = require("../index");
 const jobs = require("../jobs");
 
 const xApiKey = config.STORAGE_SECRET_KEY ? config.STORAGE_SECRET_KEY : "";
-const storageEndpoint = config.STORAGE_ENDPOINT ? config.STORAGE_ENDPOINT : "http://127.0.0.1:8001";
+const storageEndpoint = config.STORAGE_ENDPOINT
+  ? config.STORAGE_ENDPOINT
+  : "http://127.0.0.1:8001";
 
 agenda.define(jobs.PORTAL_INDEX, async (job, done) => {
-    console.log("Running job", jobs.PORTAL_INDEX);
-    const url = storageEndpoint + '/portal/index/trigger';
-    const myHeaders = new Headers();
-    myHeaders.append("x-api-key", xApiKey);
+  console.log("Running job", jobs.PORTAL_INDEX);
+  const url = storageEndpoint + "/portal/index/trigger";
+  const myHeaders = new Headers();
+  myHeaders.append("x-secret-key", xApiKey);
 
-    const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow"
-    };
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
+  try {
+    fetch(url, requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-    try {
-        fetch(url, requestOptions)
-            .then((response) => response.text())
-            .then((result) => {
-                console.log(result)
-            }).catch((error) => {
-                console.error(error)
-            });
-
-        done();
-    } catch (err) {
-        console.error("Error in job", jobs.PORTAL_INDEX, err);
-        done(err);
-    } finally {
-        console.log("Job done", jobs.PORTAL_INDEX);
-    }
+    done();
+  } catch (err) {
+    console.error("Error in job", jobs.PORTAL_INDEX, err);
+    done(err);
+  } finally {
+    console.log("Job done", jobs.PORTAL_INDEX);
+  }
 });
