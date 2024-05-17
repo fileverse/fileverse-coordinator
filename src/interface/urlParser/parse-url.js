77d3ab87a2d03964d63172da88e99fbc1d57f251
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-05-17 18:19:17 +0530

    feat: added farcaster url fetching in parseUrl endpoint

--- src/interface/urlParser/parse-url.js
+++ src/interface/urlParser/parse-url.js
@@ -1,28 +1,41 @@
 
 const urlMetadata = require('url-metadata');
+const warpcast = require('./warpcast');
+
+
+async function parser(url) {
+    if (url.match(/^https?:\/\/(www\.)?warpcast.com\/\d+\/\w+/)) {
+        return ["warpcast::cast", await warpcast.getCastMetadata(url)];
+    } else if (url.match(/^https?:\/\/(www\.)?warpcast.com\/~\/.+/)) {
+        return ["warpcast::channel", await warpcast.getChannelMetadata(url)];
+    } else if (url.match(/^https?:\/\/(www\.)?warpcast.com\/.+/)) {
+        return ["warpcast::profile", await warpcast.getProfileMetadata(url)];
+    } else {
+        return ["unidentified", await urlMetadata(url)];
+    }
+}
 
 async function parseUrl(req, res) {
     const { url } = req.query;
+    if (!url) {
+        return res.status(400).json({ error: 'url is required' });
+    }
+
+    let resp = {
+        urlType: "",
+        data: {},
+        error: null,
+    }
+
     try {
-        const metadata = await urlMetadata(url);
-
-        let resp = {
-            favicons: metadata?.favicons,
-            url: metadata?.url,
-            title: metadata?.title,
-            description: metadata?.description,
-        }
-
-        Object.entries(metadata).map(([key, value]) => {
-            if (key.startsWith('og:')) {
-                resp[key] = value
-            }
-        });
-
-        return res.json(resp);
+        const [urlType, data] = await parser(url);
+        resp.data = data;
+        resp.urlType = urlType;
+        return res.status(200).json(resp);
     } catch (err) {
         console.log(err);
-        return res.status(500).json({ error: 'Error parsing url: ' + err.message });
+        resp.error = 'Error parsing url: ' + err.message
+        return res.status(500).json(resp);
     }
 
 }
