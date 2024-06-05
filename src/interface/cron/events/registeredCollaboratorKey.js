58655d54b2e8b3620e7f6e7477cdf77713bbe49e
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-06-05 18:47:34 +0530

    missing import fixed

--- src/interface/cron/events/registeredCollaboratorKey.js
+++ src/interface/cron/events/registeredCollaboratorKey.js
@@ -1,7 +1,7 @@
 const config = require("../../../../config");
 const constants = require("../../../constants");
 const { EventProcessor, Event } = require("../../../infra/database/models");
-const fetchAddedEventsID = require("./utils");
+const EventUtil = require("./utils");
 const agenda = require("../index");
 const jobs = require("../jobs");
 const axios = require("axios");
@@ -45,7 +45,7 @@ async function fetchRegisteredCollaboratorKeyCheckpoint() {
 }
 
 async function fetchRegisteredCollaboratorKeyEvents(checkpoint, itemCount) {
-  const existingEventIds = await fetchAddedEventsID(EVENT_NAME);
+  const existingEventIds = await EventUtil.fetchAddedEventsID(EVENT_NAME);
   const response = await axios.post(API_URL, {
     query: `{
       ${EVENT_NAME}(first: ${itemCount || 5}, orderDirection: asc, orderBy: blockNumber,
